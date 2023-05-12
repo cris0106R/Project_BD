82125f5c65e625ded7f07065136605aec0dd1a66
haowill05 <133372665+haowill05@users.noreply.github.com>
2023-05-12 16:43:43 +0200

    Create utilities.py

---
+++ utilities.py
@@ -0,0 +1,26 @@
+# Get all the games
+def getGames(gameid, option="", getall=False):
+        if getall:
+                query = f"SELECT {option} from Game"
+        else:
+                query = f"SELECT {option} FROM Game WHERE IdGame=\'{gameid}\'"
+        return dbquery(query)
+
+def getUsers(userid, option="", getall=False):
+        if getall:
+                query = f"SELECT {option} from User"
+        else:
+                query = f"SELECT {option} FROM User WHERE IdUser = \'{userid}\'"
+
+        return dbquery(query)
+
+def getUserid(email):
+        return dbquery(f"SELECT IdUser FROM User WHERE email = \'{email}\'")[0]
+
+def authenticate():
+        if not session.get('userid'):
+                return False
+        return True
+
+def error(message):
+        return f"<script>alert(\'{message}\');window.location.assign(\'http://localhost:5000/\')</script>"
