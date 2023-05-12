d36b1349491d84c0875959977c08e2686200b3e4
haowill05 <133372665+haowill05@users.noreply.github.com>
2023-05-13 01:24:12 +0200

    added coherent functions

--- utilities.py
+++ utilities.py
@@ -12,52 +12,69 @@ connection = connector.connect(
 
 # * sql query wrapper
 def dbquery(q, action="select"):
-    result = []
-    with connection.cursor() as cursor:
-        cursor.execute(q)
-        if action != "select":
-            connection.commit()  # either update or insert
-        else:
-            rows = cursor.fetchall()
-            if rows:
-                for r in rows:
-                    result.append(r)
-                return result[0][0]
-
-def getAllGames():
-    return ""
+        result = []
+        with connection.cursor() as cursor:
+                cursor.execute(q)
+                if action != "select":
+                        connection.commit() # either update or insert
+                else:
+                        rows = cursor.fetchall()
+                        if rows:
+                                for r in rows:
+                                        result.append(r)
+                                return result # <-- result is an array, to access first row use result[0], to access first column value of first row use result[0][0]
 
 
-def getGames(gameid, option="", getall=False):
-    if getall:
-        query = f"SELECT {option} FROM Game"
-    else:
-        query = f"SELECT {option} FROM Game WHERE IdGame=\'{gameid}\'"
-    return dbquery(query)
+# Game related helper functions:
+def getGames():
+	query = f"SELECT game_title FROM Game"
+	result = dbquery(query)
+	games = []
 
+	for i in len(result):
+		games.append(result[i][0])
+	return games
 
-def getUsers(userid, option="", getall=False):
-    if getall:
-        query = f"SELECT {option} from User"
-    else:
-        query = f"SELECT {option} FROM User WHERE IdUser = \'{userid}\'"
+def getGameid(game_title):
+	query = f"SELECT IdGame FROM Game WHERE game_title = \'{game_title}\'"
+	return dbquery(query)[0][0]
 
-    return dbquery(query)
+def getGametitle(gameid):
+	query = f"SELECT game_title FROM Game WHERE IdGame = {gameid}"
+	return dbquery(query)[0][0]
 
+# User related helper functions:
+def getUsers():
+	query = f"SELECT name from User"
+	result = dbquery(query)
+	usernames = []
+
+	for i in len(result):
+		usernames.append(usernames[i][0])
+	return usernames
+	
 
 def getUserid(email):
-    return dbquery(f"SELECT IdUser FROM User WHERE email = \'{email}\'")[0]
+	query = f"SELECT IdUser FROM User WHERE email = \'{email}\'"
+	return dbquery(query)[0][0]
 
+def getUsername(userid):
+	query = f"SELECT name FROM User WHERE IdUser = {userid}"
+	return dbquery(query)[0][0]
 
+# Session related helper functions:
 def setSession(session, userid):
-    session['userid'] = userid
+	session['userid'] = userid
 
+def unsetSession(session):
+	session['userid'] = None
 
 def authenticate(session):
-    if not session.get('userid'):
-        return False
-    return True
+	if not session.get('userid'):
+		return False
+	return True
+
 
 
 def error(message):
-    return f"<script>alert(\'{message}\');window.location.assign(\'http://127.0.0.1:5000/index\')</script>"
+	return f"<script>alert(\'{message}\');window.location.assign(\'http://localhost:5000/\')</script>"
